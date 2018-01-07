cb9cd37342201496dea92dc1539b8d396aec9da0
Animesh Jain <anijain.0@gmail.com>
2018-01-07 20:34:38 +0530

    Initial commit

---
+++ app/src/main/java/com/animeshjain/assignmnet191/network/OnWebServiceResult.java
@@ -0,0 +1,9 @@
+package com.animeshjain.assignmnet191.network;
+
+
+import com.animeshjain.assignmnet191.utils.CommonUtilities;
+
+
+public interface OnWebServiceResult {
+    public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type);
+}
---
+++ app/src/main/java/com/animeshjain/assignmnet191/utils/CommonUtilities.java
@@ -0,0 +1,10 @@
+package com.animeshjain.assignmnet191.utils;
+
+public class CommonUtilities {
+
+    public enum SERVICE_TYPE {
+        GET_DATA,
+        ERROR_REPORT
+
+    }
+}
